5d8abc67f186d67547884dad9d2b016066c15eb7
baxy5 <bakos.soda@gmail.com>
2022-12-08 18:58:31 +0100

    nodejs latest lts and current version

--- webs.py
+++ webs.py
@@ -1,27 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-nodejs_url='https://nodejs.org/en/download/releases/'
+nodejs_lts_url = 'https://nodejs.org/en/download/'
+nodejs_latest_url = 'https://nodejs.org/en/download/releases/'
 strapi_url='https://strapi.io/changelog'
 
-print("Updated: yesterday")
 
-# NodeJS
-response = requests.get(nodejs_url)
+# NodeJS - LTS
+response = requests.get(nodejs_lts_url)
 soup = BeautifulSoup(response.text, 'html.parser')
-nodejs = soup.tbody
-c = 0
-for tr in nodejs:
-    for td in tr:
-        if td.select_one("LTS"):
-            break
-        print(td)
-print(nodejs_version)
-#nodejs_version = str(nodejs.select_one(":nth-child(5)"))
-#nodejs_version_date = str(nodejs.select_one(":nth-child(3)").getText())
-#strong_begin = nodejs_version.find("<strong>")
-#strong_end = nodejs_version.find("</strong>")
-#print("Latest NodeJS Version: ", nodejs_version[(strong_begin+8):strong_end])
+nodejs_lts_version = soup.p.getText()
+print("NodeJS: " , nodejs_lts_version)
+# NodeJS - LATEST
+response = requests.get(nodejs_latest_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+nodejs_latest_version = soup.tbody
+print("NodeJS (LATEST CURRENT): ", nodejs_latest_version.tr.td.getText(), "(", nodejs_latest_version.tr.time.getText(), ")")
 
 #Strapi
 response = requests.get(strapi_url)
@@ -35,4 +29,4 @@ strapi_date = str(soup.find("span", {"class": "typography_Label__1knAy"}))
 strapi_date_begin = strapi_date.find(">")
 strapi_date_end = strapi_date.find("</")
 
-print("Latest Strapi Version: %s (%s)" % (strapi_version[strapi_begin:strapi_end] , strapi_date[(strapi_date_begin+1):strapi_date_end]))
+print("Strapi: %s (%s)" % (strapi_version[strapi_begin:strapi_end] , strapi_date[(strapi_date_begin+1):strapi_date_end]))
